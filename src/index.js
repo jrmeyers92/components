437781116e5dc44d48750433145e394036027a51
Jake Meyers <JakeRMeyers92@gmail.com>
2020-09-08 20:33:02 -0600

    more components with props

--- src/index.js
+++ src/index.js
@@ -2,15 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from "./ApprovalCard"
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail />
-            <CommentDetail />
-            <CommentDetail />
-            <CommentDetail />
-            <CommentDetail />
+            <ApprovalCard>
+                <div className="">
+                    <h4>Warning!</h4>
+                    are you sure you want to do this?
+                </div>
+            </ApprovalCard>
+
+              <ApprovalCard >
+                <CommentDetail author="Sam" post="sick post brah!" timeAgo="today at 4:45PM" avatar = {faker.image.avatar()}/>
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail author="Alex" post="far out maane" timeAgo="today at 2:22PM" avatar = {faker.image.avatar()} />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail author="Jane" post="so dope" timeAgo="yeseterday" avatar = {faker.image.avatar()}/>
+            </ApprovalCard>
+          
             
         </div>
     )
